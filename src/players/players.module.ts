28b1c814fa864f92abf4bcbf28ea57c6cb52b296
munergenis <munergenis@gmail.com>
2024-12-01 12:20:42 +0100

    feat: adds players resource

--- src/players/players.module.ts
+++ src/players/players.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { PlayersService } from './players.service';
 import { PlayersController } from './players.controller';
-import { PlayersRepository } from './players.repository/players.repository';
+import { PlayersRepository } from './players.repository';
 
 @Module({
   controllers: [PlayersController],
